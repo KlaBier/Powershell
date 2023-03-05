2038028c0578963b104510feb70b2d0cbeee7e76
Klaus Bierschenk <klaus@nothingbutcloud.com>
2023-03-05 19:06:41 +0100

    Initial Setup

---
+++ AAD_Secret_Infos/SecretList_New.ps1
@@ -0,0 +1,42 @@
+﻿
+# https://blog.icewolf.ch/archive/2022/01/28/powershell-script-to-check-for-aad-apps-with-expiring-clientsecrets.aspx
+
+
+connect-azuread
+
+#Get Azure AD Apps
+$AzureADApps =Get-AzureADApplication -All $true
+
+Write-output "Displayname, AppID, Secret Startdate, Secret expire" | Out-File -filepath c:\temp\JustSecrets.txt
+
+#Loop through Azure AD Apps
+Foreach ($App in $AzureADApps)
+{
+    #Show App Owner UPN/Mail
+    $AppObjectID = $App.ObjectID
+    $AppOwners = Get-AzureADApplicationOwner -ObjectId $AppObjectID
+        
+    Foreach ($AppOwner in $AppOwners)
+    {
+        Foreach ($PasswordCredential in $App.PasswordCredentials)
+        {
+               Write-Output "$($App.DisplayName), $($App.AppID), s$($PasswordCredential.startdate.ToString('yyyy-MM-dd')), $($PasswordCredential.enddate.ToString('yyyy-MM-dd'))" | Out-File "c:\temp\JustSecrets.txt" -Append
+        }
+    }
+ }     
+
+
+Write-output "Displayname, AppID, Owner, Secret Startdate, Secret expire" | Out-File -filepath c:\temp\SecretsOneOwner.txt
+
+#Loop through Azure AD Apps
+Foreach ($App in $AzureADApps)
+{
+    #Show App Owner UPN/Mail
+    $AppObjectID = $App.ObjectID
+    $AppOwners = Get-AzureADApplicationOwner -ObjectId $AppObjectID
+        
+    Foreach ($PasswordCredential in $App.PasswordCredentials)
+    {
+      Write-Output "$($App.DisplayName), $($App.AppID), $($AppOwner.UserPrincipalName), $($PasswordCredential.startdate.ToString('yyyy-MM-dd')), $($PasswordCredential.enddate.ToString('yyyy-MM-dd'))" | Out-File "c:\temp\SecretsOneOwner.txt" -Append
+    }
+ }     
