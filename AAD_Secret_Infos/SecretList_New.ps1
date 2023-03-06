9d6d79ba92a66e0d433d4e5ee7b5f7a363d78bac
Klaus Bierschenk <klaus@nothingbutcloud.com>
2023-03-06 19:38:18 +0100

    Changed Fileinfos

--- AAD_Secret_Infos/SecretList_New.ps1
+++ AAD_Secret_Infos/SecretList_New.ps1
@@ -1,29 +1,32 @@
 ﻿
 connect-azuread
 
-#Get Azure AD Apps
+$ExportPath = ".\AAD_Secret_Infos"
+
+# Get Azure AD Apps
 $AzureADApps =Get-AzureADApplication -All $true
 
-Write-output "Displayname, AppID, Secret Startdate, Secret expire" | Out-File -filepath .\AAD_Secret_Infos\SecretsOnly.txt
+Write-output "Displayname, AppID, Secret ID, Secret Startdate, Secret expire" | Out-File -FilePath ($ExportPath+"\AllAppOwnerAllSecrets.txt")
 
-#Loop through Azure AD Apps
+# run through all Azure AD Apps
 Foreach ($App in $AzureADApps)
 {
-    #Show App Owner UPN/Mail
+    # Get Owner
     $AppObjectID = $App.ObjectID
     $AppOwners = Get-AzureADApplicationOwner -ObjectId $AppObjectID
-        
+    
+    # Write all owner to file
     Foreach ($AppOwner in $AppOwners)
     {
         Foreach ($PasswordCredential in $App.PasswordCredentials)
         {
-               Write-Output "$($App.DisplayName), $($App.AppID), $($PasswordCredential.startdate.ToString('yyyy-MM-dd')), $($PasswordCredential.enddate.ToString('yyyy-MM-dd'))" | Out-File ".\AAD_Secret_Infos\SecretsOnly.txt" -Append
+               Write-Output "$($App.DisplayName), $($App.AppID), $($PasswordCredential.KeyId) $($PasswordCredential.startdate.ToString('yyyy-MM-dd')), $($PasswordCredential.enddate.ToString('yyyy-MM-dd'))" | Out-File -FilePath ($ExportPath+"\AllAppOwnerAllSecrets.txt") -Append
         }
     }
  }     
 
 
-Write-output "Displayname, AppID, Owner, Secret Startdate, Secret expire" | Out-File -filepath .\AAD_Secret_Infos\SecretsOneOwner.txt
+Write-output "Displayname, AppID, Owner, Secret Startdate, Secret expire" | Out-File -filepath ($ExportPath+"\OneAppOwnerAllSecrets.txt")
 
 #Loop through Azure AD Apps
 Foreach ($App in $AzureADApps)
@@ -34,6 +37,7 @@ Foreach ($App in $AzureADApps)
         
     Foreach ($PasswordCredential in $App.PasswordCredentials)
     {
-      Write-Output "$($App.DisplayName), $($App.AppID), $($AppOwner.UserPrincipalName), $($PasswordCredential.startdate.ToString('yyyy-MM-dd')), $($PasswordCredential.enddate.ToString('yyyy-MM-dd'))" | Out-File ".\AAD_Secret_Infos\SecretsOneOwner.txt" -Append
+      Write-Output "$($App.DisplayName), $($App.AppID), $($AppOwner.UserPrincipalName), $($PasswordCredential.startdate.ToString('yyyy-MM-dd')), $($PasswordCredential.enddate.ToString('yyyy-MM-dd'))" | Out-File -FilePath ($ExportPath+"\OneAppOwnerAllSecrets.txt") -Append
     }
  }     
+ 
