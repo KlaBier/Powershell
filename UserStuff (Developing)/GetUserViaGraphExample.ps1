752a205d6d15f8721d18eca1971e84a7c6f8142e
Klaus Bierschenk <klaus@nothingbutcloud.com>
2023-03-06 12:41:09 +0100

    some script updates

---
+++ UserStuff (Developing)/GetUserViaGraphExample.ps1
@@ -0,0 +1,10 @@
+﻿# required module
+Install-Module Microsoft.Graph
+
+Connect-MgGraph -Scopes 'User.Read.All'
+Get-MgUser -ConsistencyLevel eventual -Count userCount -Filter "startsWith(DisplayName, 'a')" -Top 1
+
+Get-MgUser -ConsistencyLevel eventual -Count userCount
+
+
+Get-MgUser -UserId "AdeleV@kbcorp2021.onmicrosoft.com"| select *
