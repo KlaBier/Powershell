0b55de6a862d02cba887c75f682e704fd4e90ea9
Klaus Bierschenk <klaus@nothingbutcloud.com>
2023-08-17 11:19:20 +0200

    Fixed som typos

--- Administrative Units/AU_Stuff.ps1
+++ Administrative Units/AU_Stuff.ps1
@@ -1,18 +1,16 @@
 # Beispiele für den Umgang mit Administrative Units
 
-# Weitere Hinweise finden sich in dem Artikel
-# https://learn.microsoft.com/en-us/graph/api/resources/administrativeunit?view=graph-rest-1.0
+# Ideen und Hinweise hier https://learn.microsoft.com/en-us/graph/api/resources/administrativeunit?view=graph-rest-1.0
 
 Import-Module Microsoft.Graph.Identity.DirectoryManagement
 
 $params = @{
-	displayName = "Praktikanten3"
+	displayName = "Praktikanten"
 	description = "Enthält alle Benutzerkonten der Praktikanten"
 	visibility = "HiddenMembership"
 }
 
 Connect-MgGraph -scopes "AdministrativeUnit.ReadWrite.All"
-
 
 New-MgDirectoryAdministrativeUnit -BodyParameter $params
 
