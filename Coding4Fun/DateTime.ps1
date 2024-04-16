c5d69559627622bca20be4dbc393114b325ffbfe
Klaus Bierschenk <klaus.nothingbutcloud.net>
2024-04-16 17:33:23 +0200

    minor changes

--- Coding4Fun/DateTime.ps1
+++ Coding4Fun/DateTime.ps1
@@ -1,4 +1,24 @@
 # test with d_t
-$time = [DateTime]::UtcNow | get-date -Format "yyyy-MM-ddTHH:mm:ssZ"
+$time = [DateTime]::UtcNow | get-date -Format "yyyy-MM-ddTHH:mm:fZ"
 
 $time
+
+FormatDateTime($time, , "yyyy-MM-ddzzz", "yyyyMMddHHmmss.fZ")
+
+‘20221101010000.0Z’
+
+[DateTime]::UtcNow
+
+[DateTime]
+
+Install-Modulae -name microsoft.graph
+
+# Login/Connect to MS Graph
+Connect-MgGraph -Scopes 'Application.Read.All'
+
+Import-Module Microsoft.Graph.Applications
+
+Get-MgUserAppRoleAssignment -UserId 93e77d38-5344-4481-8dd9-4cb19a23bca0
+
+Get-GraphAppRoleAssignment -UserId 93e77d38-5344-4481-8dd9-4cb19a23bca0
+
