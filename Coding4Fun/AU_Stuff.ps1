6c2fcea705228980cf906c4d02550c445da34d25
Klaus Bierschenk <klaus@nothingbutcloud.com>
2025-03-18 13:12:43 +0100

    add some files

---
+++ Coding4Fun/AU_Stuff.ps1
@@ -0,0 +1,20 @@
+# Beispiele für den Umgang mit Administrative Units
+
+# Ideen und Hinweise hier https://learn.microsoft.com/en-us/graph/api/resources/administrativeunit?view=graph-rest-1.0
+
+Import-Module Microsoft.Graph.Identity.DirectoryManagement
+
+$params = @{
+	displayName = "Praktikanten"
+	description = "Enthält alle Benutzerkonten der Praktikanten"
+	visibility = "HiddenMembership"
+}
+
+Connect-MgGraph -scopes "AdministrativeUnit.ReadWrite.All" -verbose
+
+New-MgDirectoryAdministrativeUnit -BodyParameter $params
+
+Disconnect-Graph
+
+Install-Module Microsoft365DSC -Force
+
