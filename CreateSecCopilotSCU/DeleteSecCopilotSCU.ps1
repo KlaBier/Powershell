5af8aef38b7731cdeb55607257ff15c99e211a86
Klaus Bierschenk <klaus@nothingbutcloud.com>
2026-01-20 11:51:25 +0000

    Update SKU Cmdlets

--- CreateSecCopilotSCU/DeleteSecCopilotSCU.ps1
+++ CreateSecCopilotSCU/DeleteSecCopilotSCU.ps1
@@ -1,3 +1,5 @@
 # Quick delete the entire RG with the associated SCU
 
+Connect-AzAccount -Tenant 'YOUR TENANT ID' -SubscriptionId 'YOUR SUBSCRIPTION ID'
+
 Remove-AzResourceGroup -Name "SecurityCopilotRG" -force
